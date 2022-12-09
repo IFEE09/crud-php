0365776cdeedb701b116f100aa6adbd9f83e59c8
IFEE09 <ivanfaridespadas@gmail.com>
2022-12-09 09:11:20 -0600

    terminando el form

--- index.php
+++ index.php
@@ -4,5 +4,26 @@
 <html lang="en">
 
 <?php include("includes/header.php") ?>
-    <h1>Hello world!</h1>
+
+    <div class="container p-4">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card card-body">
+                    <form action="">
+                        <div class="form-group">
+                            <input type="text" name="title" class="form-control" placeholder="Task Title" autofocus>
+                        </div> <br>
+                        <div class="form-group">
+                            <textarea name="description" rows="2" class="form-control" placeholder="Task Description"></textarea>
+                        </div> <br>
+                        <input class="btn btn-success btn-block" type="submit" name="save_task" value="Save Task">
+                    </form>
+                </div>
+            </div>
+            <div class="col-md-8">
+
+            </div>
+        </div>
+    </div>
+
 <?php include("includes/footer.php") ?>
