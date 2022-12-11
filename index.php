e2b1210f036ba1d77d1bcfd777a2e555ebb5afa7
IFEE09 <ivanfaridespadas@gmail.com>
2022-12-10 19:40:40 -0600

    agregando espacios

--- index.php
+++ index.php
@@ -10,12 +10,15 @@
             <div class="col-md-4">
                 <div class="card card-body">
                     <form action="save_task.php" method="POST">
+                        
                         <div class="form-group">
                             <input type="text" name="title" class="form-control" placeholder="Task Title" autofocus>
                         </div> <br>
+
                         <div class="form-group">
                             <textarea name="description" rows="2" class="form-control" placeholder="Task Description"></textarea>
                         </div> <br>
+
                         <input class="btn btn-success btn-block" type="submit" name="save_task" value="Save Task">
                     </form>
                 </div>
