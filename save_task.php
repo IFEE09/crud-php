8f2f56ef13b52c56f8bd44938e5d3e87490eb32b
IFEE09 <ivanfaridespadas@gmail.com>
2022-12-10 19:53:32 -0600

    agregando primer save_task

---
+++ save_task.php
@@ -0,0 +1,21 @@
+<?php
+use LDAP\Result;
+
+include("db.php");
+
+if(isset($_POST['save_task'])) {
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    echo $title . ' ' . $description;
+
+    $query = "INSERT INTO task(title, description) VALUES ('$title', '$description')";
+    $result = mysqli_query($conn, $query);
+    
+    if(!$result) {
+        die("Query Failed");
+    } else {
+        echo "saved";
+    }
+}
+
+?>
