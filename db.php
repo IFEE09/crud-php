80b2ca93e091c56ab08bb1346ea011f84b3de262
IFEE09 <ivanfaridespadas@gmail.com>
2022-12-14 15:47:26 -0600

    conectando la base de datos exitosamente

--- db.php
+++ db.php
@@ -1,9 +1,23 @@
 <?php
-    $conn = mysqli_connect(
-        'localhost:8888',
-        'root',
-        '',
-        'php_mysql_crud'
-    );
-?>
 
+$host = "localhost:3306";
+$dbName = "nombres";
+$userName = "root";
+$password = "12345678";
+
+$mysqli = new mysqli(
+    hostname: $host,
+    username: $userName,
+    password: $password,
+    database: $dbName
+);
+
+if ($mysqli->connect_errno) {
+    die("Connection error: " . $mysqli->connect_error);
+} else {
+    echo "database is connected!";
+}
+
+return $mysqli;
+
+?>
