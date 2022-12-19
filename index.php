4f48175c8f5d3e70196dfc6f13f48ddbdb2ebfbe
IFEE09 <ivanfaridespadas@gmail.com>
2022-12-19 05:16:49 -0600

    agregando primeras lineas en save_task.php

--- index.php
+++ index.php
@@ -9,18 +9,17 @@
         <div class="row">
             <div class="col-md-4">
                 <div class="card card-body">
-                    <form action="save_task.php" method="POST">
-                        
+
+                    <form action="save_task.php" method="POST">    
                         <div class="form-group">
                             <input type="text" name="title" class="form-control" placeholder="Task Title" autofocus>
                         </div> <br>
-
                         <div class="form-group">
                             <textarea name="description" rows="2" class="form-control" placeholder="Task Description"></textarea>
                         </div> <br>
-
                         <input class="btn btn-success btn-block" type="submit" name="save_task" value="Save Task">
                     </form>
+                    
                 </div>
             </div>
             <div class="col-md-8">
--- save_task.php
+++ save_task.php
@@ -1,22 +1,9 @@
 <?php
-use LDAP\Result;
-
-include("db.php");
 
 if(isset($_POST['save_task'])) {
     $title = $_POST['title'];
     $description = $_POST['description'];
     echo $title . ' ' . $description;
-
-    $query = "INSERT INTO task(title, description) VALUES ('$title', '$description')";
-    $conn = require __DIR__ . "/db.php";
-    $result = mysqli_query($conn, $query);
-    
-    if(!$result) {
-        die("Query Failed");
-    } else {
-        echo "saved";
-    }
 }
 
 ?>
