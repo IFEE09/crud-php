22a33e485fb34ff96f812dba2a1a92c2a0acbe44
IFEE09 <ivanfaridespadas@gmail.com>
2022-12-19 05:22:21 -0600

    consulta hecha correctamente

--- save_task.php
+++ save_task.php
@@ -1,9 +1,21 @@
 <?php
 
+include('db.php');
+
 if(isset($_POST['save_task'])) {
     $title = $_POST['title'];
     $description = $_POST['description'];
     echo $title . ' ' . $description;
+    //Consulta para la base de datos
+    $query = "INSERT INTO task (title, description) values ('$title', '$description')";
+    $result = mysqli_query($conn, $query);
+
+    if(!$result) {
+        die("Query Failed");
+    } else {
+        echo 'Saved in database';
+    }
+
 }
 
 ?>
