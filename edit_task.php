8d635fae3d043daf72d1890f6b9945889fedfe27
IFEE09 <ivanfaridespadas@gmail.com>
2022-12-20 07:07:35 -0600

    solucionando bug de edit_task

--- edit_task.php
+++ edit_task.php
@@ -2,5 +2,14 @@
 
 include("db.php");
 
+if(isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $query = "SELECT * FROM task WHERE id = $id";
+    $result = mysqli_query($conn, $query);
+    if(mysqli_num_rows($result) == 1) {
+        echo "You Can Edit";
+    }
+
+}
 
 ?>
