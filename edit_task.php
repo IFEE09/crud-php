3863900dd98c62c4c8bdf7a4a8ac248751adae19
IFEE09 <ivanfaridespadas@gmail.com>
2022-12-20 08:23:06 -0600

    finalizando crud

--- edit_task.php
+++ edit_task.php
@@ -2,16 +2,26 @@
 
 include("db.php");
 
-if(isset($_GET['id'])) {
+if (isset($_GET['id'])) {
     $id = $_GET['id'];
     $query = "SELECT * FROM task WHERE id = $id";
     $result = mysqli_query($conn, $query);
-    if(mysqli_num_rows($result) == 1) {
+    if (mysqli_num_rows($result) == 1) {
         $row = mysqli_fetch_array($result);
         $title = $row['title'];
         $description = $row['description'];
     }
 
+}
+
+if (isset($_POST['update'])) {
+    $id = $_GET['id'];
+    $title = $_POST['title'];
+    $description = $_POST['title'];
+
+    $query = "UPDATE task set title = '$title', description = '$description' WHERE id = $id";
+    mysqli_query($conn, $query);
+    header("Location: index.php");
 }
 
 ?>
@@ -22,14 +32,13 @@ if(isset($_GET['id'])) {
     <div class="row">
         <div class="col-md-4 mx-auto">
             <div class="card card-body">
-                <form action="edit.php?id=<?php echo $_GET['id']; ?>" method="POST">
+                <form action="edit_task.php?id=<?php echo $_GET['id']; ?>" method="POST">
                     <div class="form-group">
-                        <input type="text" name="title" value="<?php echo "title"; ?>" class="form-control" placeholder="Update Title"> <br>
-                    </div>
+                        <input type="text" name="title" value="<?php echo "title"; ?>" class="form-control"
+                            placeholder="Update Title">
+                    </div> <br>
                     <div class="form-group">
-                        <textarea name="description" id="" rows="2" cols="34" rows="10">
-                            <?php echo $description; ?>
-                        </textarea>
+                        <textarea name="description" id="" rows="2" cols="34" rows="10"></textarea>
                     </div> <br>
                     <button class="btn btn-success" name="update">
                         Update
