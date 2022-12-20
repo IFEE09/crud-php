e97a624f1cfd98e2cbdcab0ddae166139fd45245
IFEE09 <ivanfaridespadas@gmail.com>
2022-12-20 08:03:03 -0600

    agregando cosas nuevas en edit_task

--- edit_task.php
+++ edit_task.php
@@ -7,9 +7,37 @@ if(isset($_GET['id'])) {
     $query = "SELECT * FROM task WHERE id = $id";
     $result = mysqli_query($conn, $query);
     if(mysqli_num_rows($result) == 1) {
-        echo "You Can Edit";
+        $row = mysqli_fetch_array($result);
+        $title = $row['title'];
+        $description = $row['description'];
     }
 
 }
 
 ?>
+
+<?php include("includes/header.php") ?>
+
+<div class="container p-4">
+    <div class="row">
+        <div class="col-md-4 mx-auto">
+            <div class="card card-body">
+                <form action="edit.php?id=<?php echo $_GET['id']; ?>">
+                    <div class="form-group">
+                        <input type="text" name="title" value="<?php echo "title"; ?>" class="form-control" placeholder="Update Title"> <br>
+                    </div>
+                    <div class="form-group">
+                        <textarea name="description" id="" rows="2" cols="34" rows="10">
+                            <?php echo $description; ?>
+                        </textarea>
+                    </div> <br>
+                    <button class="btn btn-success" name="update">
+                        Update
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include("includes/footer.php") ?>
